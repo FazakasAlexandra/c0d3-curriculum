9b2590791514901a1b518105af6d19a367adbf2c
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2022-08-15 17:07:11 +0300

    refactor and pass initial state prop to Kanban and Stars

--- js6/p3/app.js
+++ js6/p3/app.js
@@ -1,7 +1,224 @@
 const { useState, useEffect } = React;
 
-const Kanban = () => {
-  const [kanban, setKanban] = useState(()=> JSON.parse(localStorage.getItem("kanbanState")) ?? [
+const KANBAN_DIRECTIONS = {
+  forward: "forward",
+  backward: "backward",
+};
+
+const BoardForm = ({ actions, board }) => {
+  return (
+    <form className="row">
+      <textarea
+        rows="4"
+        value={board.input}
+        onChange={(e) => actions.setInput(e.target.value)}
+      />
+      <button onClick={(_e) => actions.addToList(board.id)}>Submit</button>
+    </form>
+  );
+};
+
+const BoardItem = ({ showLeftArrow, showRightArrow, actions, children }) => {
+  return (
+    <li>
+      {(showLeftArrow && (
+        <div
+          className="arrow l"
+          onClick={() => actions.move(KANBAN_DIRECTIONS.backward)}
+        >
+          &lt;
+        </div>
+      )) ||
+        null}
+      <span className="item" onClick={() => actions.remove()}>
+        {children}
+      </span>
+      {(showRightArrow && (
+        <div
+          className="arrow r"
+          onClick={() => actions.move(KANBAN_DIRECTIONS.forward)}
+        >
+          &gt;
+        </div>
+      )) ||
+        null}
+    </li>
+  );
+};
+
+const Board = ({ board, children }) => {
+  return (
+    <div className="column">
+      <h1 style={{ background: board.color }}>{board.title}</h1>
+      {children}
+    </div>
+  );
+};
+
+const Kanban = ({ boards }) => {
+  const [kanban, setKanban] = useState(boards);
+
+  const setBoardInput = (boardId, value) => {
+    setKanban(
+      kanban.map((board) => {
+        if (board.id === boardId) {
+          console.log(value);
+          board.input = value;
+        }
+        return board;
+      })
+    );
+  };
+
+  const addItemToBoardList = (boardId) => {
+    setKanban(
+      kanban.map((board) => {
+        if (board.id === boardId) {
+          board.list.push(board.input);
+          board.input = "";
+        }
+        return board;
+      })
+    );
+  };
+
+  const moveItem = (itemIndex, boardIndex, direction) => {
+    const targetBoardIndex =
+      direction === KANBAN_DIRECTIONS.forward ? boardIndex + 1 : boardIndex - 1;
+
+    // deep clones kanban
+    const kanbanCopy = JSON.parse(JSON.stringify(kanban));
+
+    kanbanCopy[targetBoardIndex].list.push(
+      kanbanCopy[boardIndex].list.splice(itemIndex, 1)
+    );
+
+    setKanban(kanbanCopy);
+  };
+
+  const removeItem = (itemIndex, boardIndex) => {
+    if (
+      confirm(
+        "Are you absolutely certain you wanna delete this?\nIt is IRREVERSIBLE."
+      ) === true
+    ) {
+      // deep clones kanban
+      const kanbanCopy = JSON.parse(JSON.stringify(kanban));
+      kanbanCopy[boardIndex].list.splice(itemIndex, 1);
+      setKanban(kanbanCopy);
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("boards", JSON.stringify([...kanban]));
+  }, [kanban]);
+
+  return (
+    <div className="row boards">
+      {kanban.map((board, boardIndex) => {
+        return (
+          <Board key={board.id} board={board}>
+            <ul>
+              {board.list.map((item, itemIndex) => (
+                <BoardItem
+                  key={`${boardIndex}-${itemIndex}`}
+                  showLeftArrow={!!boardIndex}
+                  showRightArrow={boardIndex < kanban.length - 1}
+                  actions={{
+                    remove: () => removeItem(itemIndex, boardIndex),
+                    move: (direction) =>
+                      moveItem(itemIndex, boardIndex, direction),
+                  }}
+                >
+                  {item}
+                </BoardItem>
+              ))}
+            </ul>
+            <BoardForm
+              board={board}
+              actions={{
+                addToList: () => addItemToBoardList(board.id),
+                setInput: (value) => setBoardInput(board.id, value),
+              }}
+            />
+          </Board>
+        );
+      })}
+    </div>
+  );
+};
+
+const Star = ({ events, star }) => {
+  return <i className={`${star} fa-star`} {...events}></i>;
+};
+
+const Stars = ({ number }) => {
+  const [stars, setStars] = useState(Array(number).fill("far"));
+  const [selectedStar, setSelectedStar] = useState(null);
+  const [locked, setLocked] = useState(false);
+  const count = stars.filter((star) => star === "fas").length;
+
+  const toggleStars = (targetStarIdx) => {
+    setStars(
+      stars.map((_star, i) => {
+        if (i <= targetStarIdx) {
+          return "fas";
+        }
+        return "far";
+      })
+    );
+  };
+
+  const pluralize = (nr, word) => {
+    if (nr > 1) {
+      return `${word}s`;
+    }
+    return word;
+  };
+
+  return (
+    <div>
+      <div
+        className="row"
+        onMouseLeave={() => setLocked(false)}
+        onMouseEnter={() => setSelectedStar(null)}
+      >
+        {stars.map((star, index) => (
+          <Star
+            key={index}
+            star={star}
+            events={{
+              onMouseOver: (_event) => {
+                if (!locked) {
+                  toggleStars(index);
+                }
+              },
+              onClick: (_event) => {
+                if (!locked) {
+                  setLocked(true);
+                  setSelectedStar(index);
+                }
+              },
+            }}
+          />
+        ))}
+      </div>
+      <p>
+        {(selectedStar !== null &&
+          `You have selected ${selectedStar + 1} ${pluralize(
+            selectedStar + 1,
+            "star"
+          )}!`) ||
+          `You are selecting ${count} ${pluralize(count, "star")}!`}
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const path = window.location.pathname;
+  const lastPath = path.substring(path.lastIndexOf("/") + 1) || "/";
+  const boards = JSON.parse(localStorage.getItem("boards")) ?? [
     {
       id: 1,
       title: "To-Do",
@@ -29,185 +246,12 @@ const Kanban = () => {
       color: "yellowgreen",
       input: "",
     },
-  ]);
-
-  useEffect(() => {
-      localStorage.setItem("kanbanState", JSON.stringify([...kanban]))
-  }, [kanban])
-
-  const setStageInput = (id, value) => {
-    setKanban(
-      kanban.map((stage) => {
-        if (stage.id === id) {
-          stage.input = value;
-        }
-        return stage;
-      })
-    );
-  };
-
-  const addToStageList = (id) => {
-    setKanban(
-      kanban.map((stage) => {
-        if (stage.id === id) {
-          stage.list.push(stage.input);
-          stage.input = "";
-        }
-        return stage;
-      })
-    );
-  };
-
-  const moveItem = (itemIdx, stageIdx, forward) => {
-    const targetStageIdx = forward ? stageIdx + 1 : stageIdx - 1;
-
-    // deep clones kanban
-    const kanbanCopy = JSON.parse(JSON.stringify(kanban));
-
-    kanbanCopy[targetStageIdx].list.push(
-      kanbanCopy[stageIdx].list.splice(itemIdx, 1)
-    );
-
-    setKanban(kanbanCopy);
-  };
-
-  const removeItem = (itemIdx, stageIdx) => {
-    if (
-      confirm(
-        "Are you absolutely certain you wanna delete this?\nIt is IRREVERSIBLE."
-      ) === true
-    ) {
-      // deep clones kanban
-      const kanbanCopy = JSON.parse(JSON.stringify(kanban));
-      kanbanCopy[stageIdx].list.splice(itemIdx, 1);
-      setKanban(kanbanCopy);
-    }
-  };
-
-  return (
-    <div className="row board">
-      {kanban.map((stage, idx) => {
-        return (
-          <div className="column" key={stage.id}>
-            <h1 style={{ background: stage.color }}>{stage.title}</h1>
-            <ul>
-              {stage.list.map((item, i) => {
-                return (
-                  <li key={`${item}-${i}`}>
-                    {(idx && (
-                      <div
-                        className="arrow l"
-                        onClick={() => moveItem(i, idx, false)}
-                      >
-                        &lt;
-                      </div>
-                    )) ||
-                      null}
-                    <span className="item" onClick={() => removeItem(i, idx)}>
-                      {item}
-                    </span>
-                    {(idx < kanban.length - 1 && (
-                      <div
-                        className="arrow r"
-                        onClick={() => moveItem(i, idx, true)}
-                      >
-                        &gt;
-                      </div>
-                    )) ||
-                      null}
-                  </li>
-                );
-              })}
-            </ul>
-            <div className="row">
-              <textarea
-                rows="4"
-                value={stage.input}
-                onChange={(e) => setStageInput(stage.id, e.target.value)}
-              />
-              <button onClick={(e) => addToStageList(stage.id)}>Submit</button>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
-
-const Stars = () => {
-  const [stars, setStars] = useState(["far", "far", "far", "far", "far"]);
-  const [selectedIdx, setSelectedIdx] = useState(null);
-  const [locked, setLocked] = useState(false);
-
-  const toggleStars = (targetStarIdx) => {
-    let updatedStars = stars.map((_star, i) => {
-      if (i <= targetStarIdx) {
-        return "fas";
-      }
-      return "far";
-    });
-
-    setStars(updatedStars);
-  };
-
-  const count = stars.filter((star) => star === "fas").length;
-
-  const pluralize = (nr, word) => {
-    if (nr > 1) {
-      return `${word}s`;
-    }
-    return word;
-  };
-
-  return (
-    <div>
-      <div
-        className="row"
-        onMouseLeave={() => {
-          setLocked(false);
-        }}
-        onMouseEnter={() => {
-          setSelectedIdx(null);
-        }}
-      >
-        {stars.map((star, i) => (
-          <i
-            key={i}
-            className={`${star} fa-star`}
-            onMouseOver={() => {
-              if (!locked) {
-                toggleStars(i);
-              }
-            }}
-            onClick={() => {
-              if (!locked) {
-                setLocked(true);
-                setSelectedIdx(i);
-              }
-            }}
-          ></i>
-        ))}
-      </div>
-      <p>
-        {(selectedIdx !== null &&
-          `You have selected ${selectedIdx + 1} ${pluralize(
-            selectedIdx + 1,
-            "star"
-          )}!`) ||
-          `You are selecting ${count} ${pluralize(count, "star")}!`}
-      </p>
-    </div>
-  );
-};
-
-const App = () => {
-  let path = window.location.pathname;
-  const lastPath = path.substring(path.lastIndexOf("/") + 1) || "/";
+  ];
 
   const siteMap = {
-    "/": <Kanban />,
-    kanban: <Kanban />,
-    stars: <Stars />,
+    "/": <Kanban boards={boards} />,
+    kanban: <Kanban boards={boards} />,
+    stars: <Stars number={5} />,
   };
 
   if (siteMap[lastPath]) {
--- js6/p3/server.js
+++ js6/p3/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// servers javascript and css files
+// serves javascript and css files
 app.get('/*.(js|css)', function (request, response) {
   response.sendFile(path.resolve(__dirname, request.path.substring(1)));
 });
